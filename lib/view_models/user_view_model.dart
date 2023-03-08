b82535876782458c96b8e5c86721a54df1acdca2
0912078 <0912078@naver.com>
2023-03-08 13:01:18 +0900

    Feat: google login 함수 추가

--- lib/view_models/user_view_model.dart
+++ lib/view_models/user_view_model.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:digi_how/consts/error_messages.dart';
 import 'package:digi_how/models/user_public_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class UserViewModel {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -45,7 +46,27 @@ class UserViewModel {
     return res;
   }
 
-  //
+  Future<String> signInWithGoogle() async {
+    String res = ERROR;
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      res = SUCCESS;
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+
+  //TODO: 비밀번호 찾기 기능
 
   Future<String> createUserDbWithUidAndPutPersonalInfos({
     required String name,
